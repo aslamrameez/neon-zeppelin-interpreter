04d470ffdd8207709aa257592057a52fef7828d3
Aslam Remeez <aslam.remeez@flytxt.com>
2017-02-27 13:45:02 +0530

    initial architecture

--- src/main/java/com/flytxt/interpreter/parser/DML.java
+++ src/main/java/com/flytxt/interpreter/parser/DML.java
@@ -8,13 +8,15 @@ import java.util.regex.Pattern;
  * Created by aslam on 23/2/17.
  */
 public enum DML {
-    INSERT("INSERT\\s+INTO\\s+VALUES|values"){
+
+    INSERT("INSERT\\s+INTO\\s+VALUES"){
         public  void evaluate(NeonStore store,String dml){
         new InsertStatement(this.regex).executeDML(store,dml);
         }
     },
-    UPDATE("UPDATE \w+ SET|w+\s+ "){
-        public   void evaluate(NeonStore store,String dml){
+
+    UPDATE("UPDATE \\w+ SET|w+\\s+ "){
+        public void evaluate(NeonStore store,String dml){
             new UpdateStatement(this.regex).executeDML(store,dml);
         }
     };
